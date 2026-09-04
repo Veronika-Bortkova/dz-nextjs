7d59289ffb22daa28aa88b7c2e2eb81da07fa8cb
Veronika-Bortkova <shishiga79@gmail.com>
2026-09-04 14:59:39 +0300

    DZ-2 without scc

--- src/components/FormComponent/FormComponent.tsx
+++ src/components/FormComponent/FormComponent.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React, {useActionState} from 'react';
 import {createCar} from "@/src/server-actions/car-server-actions";
+import "./FormComponent.css"
 
 
 
@@ -8,19 +9,22 @@ const FormComponent = () => {
 
     const [state, formAction, isPending] = useActionState(createCar, null);
     return (
+        <>
+            <h2>Add Car</h2>
         <form action={formAction}>
             <input type={"text"} name={"brand"} placeholder="Brand" disabled={isPending}></input>
-            {state?.errors?.brand && (<p>{state.errors.brand[0]}</p>)}
+            <div className={"error"}>{state?.errors?.brand && (<p>{state.errors.brand[0]}</p>)}</div>
 
             <input type={"number"} name={"price"} placeholder="Price" disabled={isPending}></input>
-            {state?.errors?.price && (<p>{state.errors.price[0]}</p>)}
+            <div className={"error"}>{state?.errors?.price && (<p>{state.errors.price[0]}</p>)}</div>
 
             <input type={"number"} name={"year"} placeholder="Year" disabled={isPending}></input>
-            {state?.errors?.year && (<p>{state.errors.year[0]}</p>)}
+            <div className={"error"}>{state?.errors?.year && (<p>{state.errors.year[0]}</p>)}</div>
 
             <button type="submit" disabled={isPending}>Add car</button>
 
         </form>
+    </>
     );
 };
 
--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -8,7 +8,7 @@ export const carSchema = z.object({
         message: "Brand must contain only letters (1-20 characters)"
     }),
     price: z.number({ message: "Price must be a number" }).int({ message: "Price must be an integer" }).min(0, { message: "Price cannot be less than 0" }).max(1000000, { message: "Price cannot exceed 1,000,000" }),
-    year: z.number({ message: "Year must be a number" }).int({ message: "Year must be an integer" }).min(1990, { message: "Year must be 1990 or later" }).max(2026, { message: "Year cannot be later than 2026" })
+    year: z.number({ message: "Year must be a number" }).int({ message: "Year must be an integer" }).min(1990, { message: "Year must be 1990 or later, and no later than 2026" }).max(2026, { message: "Year cannot be later than 2026, and cannot be earlier than 1990" })
 
 });
 
